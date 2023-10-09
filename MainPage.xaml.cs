566dcb62be06514f5ab8609192e827fc88766819
Jomari1111 <129037480+Jomari1111@users.noreply.github.com>
2023-10-09 09:18:46 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using Microsoft.Maui.Controls;
+using System.Text.RegularExpressions;
+
+namespace MauiPhone;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnDial(object sender, EventArgs e)
+	{
+		try
+		{
+			string phonenumber = PhoneNum.Text.Trim();
+			string phonepattern = @"^\d{11}$";
+
+			if (Regex.IsMatch(phonenumber, phonepattern))
+			{
+				PhoneDialer.Open(phonenumber);
+			}
+			else
+			{
+				await DisplayAlert("Invalid Input", "please enter valid number", "ok");
+			}
+		}
+		catch(Exception ex)
+		{
+			await DisplayAlert("Error", ex.Message, "ok");
+
+		}
+		
+	}
+}
+
